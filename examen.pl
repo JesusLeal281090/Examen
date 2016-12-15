94e2d22224cf9261c059c5c145e7154ff2c2c3fc
Jesus Valenzuela <jesusleal281090@gmail.com>
2016-12-14 16:57:48 -0800

    se agregaron las reglas

--- examen.pl
+++ examen.pl
@@ -13,7 +13,23 @@ carmelita_montiel.
 
 esposo_de(jose_aureliano_buendia,ursula_iguaran).
 padre_de(jose_aureliano_buendia,jose_arcadio_jr).
-padre_de(jose_aureliano_buendia)
+padre_de(jose_aureliano_buendia,amaranta).
+padre_de(jose_aureliano_buendia,coronel_aureliano).
+abuelo_de(jose_aureliano_buendia,arcadio).
+abuelo_de(jose_aureliano_buendia,aureliano_jose).
+esposo_de(jose_arcadio_jr,rebeca).
+esposo_de(coronel_aureliano,remedio_moscote).
+amante_de(pilar_ternera,jose_arcadio_jr).
+amante_de(pilar_ternera,coronel_aureliano).
+madre_de(ursula_iguaran,jose_arcadio_jr).
+madre_de(ursula_iguaran,amaranta).
+madre_de(ursula_iguaran,coronel_aureliano).
+abuela_de(ursula_iguaran,arcadio).
+abuela_de(ursula_iguaran,aureliano_jose).
+esposo_de(arcadio,sofia_de_la_piedad).
+esposo_de(aureliano_jose,carmelita_montiel).
+
+
 
 
 
