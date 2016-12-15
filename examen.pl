81bf8714869aa2e442eacc1393ea02636e45ea8b
Jesus Valenzuela <jesusleal281090@gmail.com>
2016-12-14 17:05:19 -0800

    se agregaron mas familiares

--- examen.pl
+++ examen.pl
@@ -20,7 +20,7 @@ abuelo_de(jose_aureliano_buendia,aureliano_jose).
 esposo_de(jose_arcadio_jr,rebeca).
 esposo_de(coronel_aureliano,remedio_moscote).
 amante_de(pilar_ternera,jose_arcadio_jr).
-amante_de(pilar_ternera,coronel_aureliano).
+amante1_de(pilar_ternera,coronel_aureliano).
 madre_de(ursula_iguaran,jose_arcadio_jr).
 madre_de(ursula_iguaran,amaranta).
 madre_de(ursula_iguaran,coronel_aureliano).
@@ -28,6 +28,10 @@ abuela_de(ursula_iguaran,arcadio).
 abuela_de(ursula_iguaran,aureliano_jose).
 esposo_de(arcadio,sofia_de_la_piedad).
 esposo_de(aureliano_jose,carmelita_montiel).
+padre_de(jose_arcadio_jr,arcadio).
+madre_de(pilar_ternera,arcadio).
+padre_de(coronel_aureliano,aureliano_jose).
+madre_de(pilar_ternera,aureliano_jose).
 
 
 
